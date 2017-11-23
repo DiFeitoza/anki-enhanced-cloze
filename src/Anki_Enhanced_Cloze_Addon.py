6bd361ca7c6e2e49545df9e12853d7a150fc94b8
Lu Zhe <luzhe610@users.noreply.github.com>
2017-11-23 21:11:44 +0800

    Major Update: Pre-release for public version

--- src/Anki_Enhanced_Cloze_Addon.py
+++ src/Anki_Enhanced_Cloze_Addon.py
@@ -13,147 +13,118 @@ from aqt.editor import Editor
 from aqt.qt import *
 
 # global variables
-genuine_answer_arr = []
-genuine_hint_arr = []
-pseudo_answer_arr = []
-pseudo_hint_arr = []
+genuine_cloze_answer_array = []
+genuine_cloze_hint_array = []
+pseudo_cloze_answer_array = []
+pseudo_cloze_hint_array = []
 current_cloze_field_number = 0
-
-hidden_block_content_arr = []
 
 # constants
 MODEL_NAME = "0 Enhanced Cloze"
 CONTENT_FIELD_NAME = "# Content"
-VALID_CLOZES_FIELD_NAME = "Valid Clozes"
+NOTE_FIELD_NAME = "Note"
+IN_USE_CLOZES_FIELD_NAME = "In-use Clozes"
+UPDATE_ENHANCED_CLOZE_SHORTCUT = "Ctrl+Shift+C"
 
 
 def generate_enhanced_cloze(note):
-    # cloze_id like c1, cloze_number like 1
+    # cloze_id means, eg. c1, cloze_number means, eg. 1
 
-    # Some specific pre-processing of content
-    src_field_name = CONTENT_FIELD_NAME
-    src_field_content = note[src_field_name]
-    # src_field_content = setup_url(src_field_content)
-    src_field_content = setup_quote(src_field_content)
-    global hidden_block_content_arr
-    del hidden_block_content_arr[:]
-    src_field_content = setup_hidden_block(src_field_content)
+    src_content = note[CONTENT_FIELD_NAME]
+    if re.search(r"\S", note[NOTE_FIELD_NAME]) != None:
+        src_content += '<br><div id="note" class="content">' + \
+            note[NOTE_FIELD_NAME] + '</div>'
 
-    # Get ids of valid clozes
+    # Get ids of in-use clozes
     cloze_start_regex = r"\{\{c\d+::"
-    cloze_start_matches = re.findall(cloze_start_regex, src_field_content)
+    cloze_start_matches = re.findall(cloze_start_regex, src_content)
 
-    # if no clozes found, empty Cloze1~Cloze99 and fill in Cloze99
+    # if no clozes are found, empty Cloze1 ~ Cloze20 and fill in Cloze99
     if not cloze_start_matches:
         for i_cloze_field_number in range(1, 20 + 1):
             dest_field_name = "Cloze%s" % i_cloze_field_number
             note[dest_field_name] = ""
 
-        note[VALID_CLOZES_FIELD_NAME] = ""
+        note[IN_USE_CLOZES_FIELD_NAME] = "[99]"
 
-        note["Cloze99"] = src_field_content + \
-            '<div style="display:none">{{c99::@@}}</div>' + \
+        # Anki will warn if cloze notes include no cloze or more strictly, no single-line cloze
+        # so I use a invisible single-line cloze {{cX::@@@@}} to cheat Anki :)
+        note["Cloze99"] = src_content + \
+            '<div style="display:none">{{c99::@@@@}}</div>' + \
             '<div id="card-cloze-id" style="display:none">c99</div>'
-        return True
+        return
     else:
         note["Cloze99"] = ""
 
-        valid_cloze_numbers = sorted(
+        in_use_clozes_numbers = sorted(
             [int(re.sub(r"\D", "", x)) for x in set(cloze_start_matches)])
-        note[VALID_CLOZES_FIELD_NAME] = str(valid_cloze_numbers)
+        note[IN_USE_CLOZES_FIELD_NAME] = str(in_use_clozes_numbers)
 
-        # Fill in content in valid cloze fields and empty content in invalid fields
+        # Fill in content in in-use cloze fields and empty content in not-in-use fields
         global current_cloze_field_number
         for current_cloze_field_number in range(1, 20 + 1):
 
             dest_field_name = "Cloze%s" % current_cloze_field_number
             dest_field_content = ""
 
-            if not (current_cloze_field_number in valid_cloze_numbers):
+            if not current_cloze_field_number in in_use_clozes_numbers:
                 dest_field_content = ""
             else:
-                # Initialize the lists
-                global genuine_answer_arr
-                global genuine_hint_arr
-                global pseudo_answer_arr
-                global pseudo_hint_arr
+                # Initialize the lists to store different content on each card later
+                global genuine_cloze_answer_array
+                global genuine_cloze_hint_array
+                global pseudo_cloze_answer_array
+                global pseudo_cloze_hint_array
 
-                del genuine_answer_arr[:]
-                del genuine_hint_arr[:]
-                del pseudo_answer_arr[:]
-                del pseudo_hint_arr[:]
+                del genuine_cloze_answer_array[:]
+                del genuine_cloze_hint_array[:]
+                del pseudo_cloze_answer_array[:]
+                del pseudo_cloze_hint_array[:]
 
-                dest_field_content = src_field_content
+                dest_field_content = src_content
 
                 cloze_regex = r"\{\{c\d+::[\s\S]*?\}\}"
                 dest_field_content = re.sub(
                     cloze_regex, process_cloze, dest_field_content)
 
-                # Store answer and hint (gunuine or pseudo) in html of every valid cloze fields
-                for index, item in enumerate(genuine_answer_arr):
-                    dest_field_content += '<pre style="display:none"><div id="genuine-answer-%s">%s</div></pre>' % (
+                # Store corresponding answers and hints (gunuine or pseudo)
+                # in html of every in-use cloze fields for javascript to fetch later
+                for index, item in enumerate(genuine_cloze_answer_array):
+                    dest_field_content += '<pre style="display:none"><div id="genuine-cloze-answer-%s">%s</div></pre>' % (
                         index, item)
-                for index, item in enumerate(genuine_hint_arr):
-                    dest_field_content += '<pre style="display:none"><div id="genuine-hint-%s">%s</div></pre>' % (
+                for index, item in enumerate(genuine_cloze_hint_array):
+                    dest_field_content += '<pre style="display:none"><div id="genuine-cloze-hint-%s">%s</div></pre>' % (
                         index, item)
-                for index, item in enumerate(pseudo_answer_arr):
-                    dest_field_content += '<pre style="display:none"><div id="pseudo-answer-%s">%s</div></pre>' % (
+                for index, item in enumerate(pseudo_cloze_answer_array):
+                    dest_field_content += '<pre style="display:none"><div id="pseudo-cloze-answer-%s">%s</div></pre>' % (
                         index, item)
-                for index, item in enumerate(pseudo_hint_arr):
-                    dest_field_content += '<pre style="display:none"><div id="pseudo-hint-%s">%s</div></pre>' % (
-                        index, item)
-
-                for index, item in enumerate(hidden_block_content_arr):
-                    dest_field_content += '<pre style="display:none"><div id="hidden-block-%s">%s</div></pre>' % (
+                for index, item in enumerate(pseudo_cloze_hint_array):
+                    dest_field_content += '<pre style="display:none"><div id="pseudo-cloze-hint-%s">%s</div></pre>' % (
                         index, item)
 
-                # Anki don't recognize multi-line clozes as valid clozes ,
-                # so just use a hidden single line cloze to pass the test
-                dest_field_content += '<div style="display:none">{{c%s::@@}}</div>' % current_cloze_field_number
+                dest_field_content += '<div style="display:none">{{c%s::@@@@}}</div>' % current_cloze_field_number
                 dest_field_content += '<div id="card-cloze-id" style="display:none">c%s</div>' % str(
                     current_cloze_field_number)
 
             note[dest_field_name] = dest_field_content
-        return True
+        return
 
 
 def check_model(model):
     return model["name"] == MODEL_NAME
 
 
-# def setup_url(html_text):
-#     return re.sub(r"(?<![\"\'])(https?|ftp)://(-\.)?([^\s/?\.#-]+\.?)+(/[^\s]*)?(?![\"\'])", r'<a href="\1">\1</a>', html_text)
-
-
-def setup_quote(html_text):
-    return re.sub(r"('''[\s\S]*?(?:</div>?)\s*?)(<div>[\s\S]*?</div>)(\s*?<div>''')", r'\1<div class="quote">\2</div>\3', html_text)
-
-
-def setup_hidden_block(html_text):
-    return re.sub(r"(--hide on--[\s\S]*?<div>)([\s\S]*?)(</div>\s*?<div>+--hide off--)", process_hidden_block_content, html_text)
-
-
-def process_hidden_block_content(matchObj):
-    global hidden_block_content_arr
-    hidden_block_content = str(matchObj.group(2))
-    hidden_block_content_arr.append(hidden_block_content)
-    index_in_arr = len(hidden_block_content_arr) - 1
-    wrapped_hidden_block_content = r'<span class="hidden-block" index="%s">%s</span>' % (
-        str(index_in_arr), hidden_block_content)
-    return str(matchObj.group(1)) + wrapped_hidden_block_content + str(matchObj.group(3))
-
-
 def process_cloze(matchObj):
 
-    cloze_string = str(matchObj.group())  # like: {{c1::aa[::bbb]}}
+    cloze_string = str(matchObj.group())  # eg. {{c1::aa[::bbb]}}
     index_of_answer = cloze_string.find("::") + 2
     index_of_hint = cloze_string.rfind("::") + 2
-    cloze_id = cloze_string[2: index_of_answer - 2]  # like: c1
+    cloze_id = cloze_string[2: index_of_answer - 2]  # like: c1 or c11
     cloze_length = len(cloze_string)
 
     answer = ""
     hint = ""
-    if (index_of_answer == index_of_hint):
+    if index_of_answer == index_of_hint:  # actually no hint at all
         answer = cloze_string[index_of_answer: cloze_length - 2]
         hint = ""
     else:
@@ -161,27 +132,27 @@ def process_cloze(matchObj):
         hint = cloze_string[index_of_hint: cloze_length - 2]
 
     global current_cloze_field_number
-    if (cloze_id != 'c' + str(current_cloze_field_number)):
+    if cloze_id != 'c' + str(current_cloze_field_number):
         # Process pseudo-cloze
-        global pseudo_answer_arr
-        global pseudo_hint_arr
-        pseudo_answer_arr.append(answer)
-        pseudo_hint_arr.append(hint)
-        index_in_arr = len(pseudo_answer_arr) - 1
+        global pseudo_cloze_answer_array
+        global pseudo_cloze_hint_array
+        pseudo_cloze_answer_array.append(answer)
+        pseudo_cloze_hint_array.append(hint)
+        index_in_array = len(pseudo_cloze_answer_array) - 1
         new_html = '<span class="pseudo-cloze" index="_index_" show-state="hint" cloze-id="_cloze-id_">_content_</span>'
-        new_html = new_html.replace('_index_', str(index_in_arr)).replace(
-            '_cloze-id_', str(cloze_id)).replace('_content_', cloze_string.replace("{", '[').replace("}", "]"))
+        new_html = new_html.replace('_index_', str(index_in_array)).replace(
+            '_cloze-id_', cloze_id).replace('_content_', cloze_string.replace("{", '[').replace("}", "]"))
         return new_html
     else:
         # Process genuine-cloze
-        global genuine_answer_arr
-        global genuine_hint_arr
-        genuine_answer_arr.append(answer)
-        genuine_hint_arr.append(hint)
-        index_in_arr = len(genuine_answer_arr) - 1
+        global genuine_cloze_answer_array
+        global genuine_cloze_hint_array
+        genuine_cloze_answer_array.append(answer)
+        genuine_cloze_hint_array.append(hint)
+        index_in_array = len(genuine_cloze_answer_array) - 1
         new_html = '<span class="genuine-cloze" index="_index_" show-state="hint" cloze-id="_cloze-id_">_content_</span>'
-        new_html = new_html.replace('_index_', str(index_in_arr)).replace(
-            '_cloze-id_', str(cloze_id)).replace('_content_', str(cloze_string))
+        new_html = new_html.replace('_index_', str(index_in_array)).replace(
+            '_cloze-id_', cloze_id).replace('_content_', cloze_string)
         return new_html
 
 
@@ -191,17 +162,15 @@ def on_add_cards(self, _old):
         return _old(self)
     generate_enhanced_cloze(note)
     ret = _old(self)
-    # tooltip('Enhanced Cloze Added')
     return ret
 
 
-def on_edit_current(self, _old):
+def on_edit_current_save(self, _old):
     note = self.editor.note
     if not note or not check_model(note.model()):
         return _old(self)
     generate_enhanced_cloze(note)
     ret = _old(self)
-    # tooltip('Enhanced Cloze Updated')
     return ret
 
 
@@ -219,19 +188,17 @@ def update_all_enhanced_clozes_in_browser(self, evt=None):
     mw.requireReset()
     mw.progress.finish()
     mw.reset()
-    # tooltip('Enhanced Clozes Updated.')
 
 
 def update_all_enhanced_cloze(self):
     mw = self.mw
-    nids = mw.col.findNotes("tag:*")
+    nids = mw.col.findNotes("*")
     for nid in nids:
         note = mw.col.getNote(nid)
         if not check_model(note.model()):
-            break
+            continue
         generate_enhanced_cloze(note)
         note.flush()
-    # tooltip('Enhanced Clozes Updated.')
 
 
 def setup_menu(self):
@@ -239,7 +206,7 @@ def setup_menu(self):
     menu = browser.form.menuEdit
     menu.addSeparator()
     a = menu.addAction('Update Enhanced Clozes')
-    a.setShortcut(QKeySequence("Ctrl+Shift+C"))
+    a.setShortcut(QKeySequence(UPDATE_ENHANCED_CLOZE_SHORTCUT))
     a.triggered.connect(
         lambda _, b=browser: update_all_enhanced_clozes_in_browser(b))
 
@@ -250,12 +217,8 @@ def on_save_now(self, callback=None):
 
 AddCards.addCards = wrap(AddCards.addCards, on_add_cards, "around")
 
-EditCurrent.onSave = wrap(EditCurrent.onSave, on_edit_current, "around")
-# Browser.closeEvent = wrap(
-#     Browser.closeEvent, update_all_enhanced_clozes_in_browser, "before")
+EditCurrent.onSave = wrap(EditCurrent.onSave, on_edit_current_save, "around")
 
 Editor.saveNow = wrap(Editor.saveNow, on_save_now, "before")
 
-addHook("browser.setupMenus", setup_menu)  # see Batch Edit add-on
-
-# addHook('editFocusLost', onFocusLost)
+addHook("browser.setupMenus", setup_menu)
